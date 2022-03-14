3e2f8588a5019b441f4dcd22ee8ac3b7c18424d4
i-jay <62462814+i-jay@users.noreply.github.com>
2022-03-14 15:32:22 +0530

    test tryon

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/6.5.1/workbox-sw.js');
-
-workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst()
-)
