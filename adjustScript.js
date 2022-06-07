af08b6d26ffeb8edcf7aebb756c88178dc8bd37b
i-jay <62462814+i-jay@users.noreply.github.com>
2022-06-07 14:51:59 +0530

    react-initial-test-video

---
+++ adjustScript.js
@@ -0,0 +1,15 @@
+// var slider = document.getElementById('slider');
+// noUiSlider.create(slider, {
+//   start: [0],
+//   direction: 'rtl',
+//   step: 1,
+//   range: {
+//     'min': -100,
+//     'max': 100
+//   }
+// });
+
+// slider.noUiSlider.on('update', function (values, handle) {
+//   sliderValue = values[0];
+//   console.log(sliderValue)
+// });
